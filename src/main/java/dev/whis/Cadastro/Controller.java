c6bffde03f03a9aa10d54b13f6a6f841fcb0e9fe
WhisRodrigues <rodrigueswhistton@gmail.com>
2025-08-11 15:51:48 -0300

    criado a Controller

---
+++ src/main/java/dev/whis/Cadastro/Controller.java
@@ -0,0 +1,27 @@
+package dev.whis.Cadastro;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class Controller {
+    @GetMapping("/boasvindas")
+    public String boasVindas() {
+        return "Essa é minha primeira rota ";
+
+
+
+
+    }
+
+
+
+
+}
+
+
+
+
+
